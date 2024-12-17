d12509ac3ce6354c9ee82b839064ccfb8a4de849
furkancanyilmaz61 <furkancaanyilmaz@gmail.com>
2024-12-17 21:01:09 +0300

    Add project files.

---
+++ HelloWorldLive/Program.cs
@@ -0,0 +1,16 @@
+﻿//See https://aka.ms/new-console-template for more information
+Console.WriteLine("Bu kismin icerisi metindir.");
+Console.WriteLine("Bu benim ilk c# uygulamam.");
+
+Console.WriteLine("------------------------------");
+
+Console.Write("Bu kismin icerisi metindir.");
+Console.Write("Bu benim ilk c# uygulamam.");
+
+Console.WriteLine("\n---------------------");
+
+Console.WriteLine("Bu ilk satir. \nBu ikinci satir.");
+
+Console.WriteLine("-------------------------------------");
+
+Console.ReadLine();
---
+++ VariablesLive/Program.cs
@@ -0,0 +1,17 @@
+﻿string isim1;
+
+isim1 = "Furkancan"; //Assignment -> atama
+                     //= ->atama operatoru
+
+int yas = 28;
+
+
+/*
+ 
+  string isim1;
+  int yas;
+  
+  isim1="furkan";
+  yas=24;
+  
+ */
